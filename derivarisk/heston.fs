43d9f2d56ef6caeb854feef91fe08482afe3e105
fran <frannuca@gmail.com>
2021-03-04 18:47:03 +0100

    First implementation of the analytical heston call/put option pricing

--- derivarisk/heston.fs
+++ derivarisk/heston.fs
@@ -5,6 +5,8 @@ open MathNet.Numerics
 open System.Numerics
 open MathNet.Numerics.Random
 open Deedle
+open System.Numerics
+open MathNet.Numerics.Integration
 
 //https://quant.stackexchange.com/questions/18684/heston-model-option-price-formula
 
@@ -118,4 +120,61 @@ open Deedle
                         )
         
         mc, cube    
-        
+
+
+    let heston_analytical(S:float,K:float,kappa:float,l:float,tau:float,r:float,q:float,rho:float,sigma:float,theta:float,vo:float,call:bool)=
+
+        let integrand(phi)=
+            let b1=Complex(kappa+l-rho*sigma,0.0)
+            let b2 = Complex(kappa + l,0.0)
+            let u1=Complex(0.5,0.0)
+            let u2= Complex(-0.5,0.0)
+            let a = Complex(kappa*theta,0.0)
+            let i= Complex(0.0,1.0)
+            let sigma2 = Complex(sigma**2.0,0.0)
+            let logK = Math.Log(K)
+            let logS = Math.Log(S)
+            let tau = Complex(tau,0.0)
+
+            let z:Complex = rho*sigma*phi*i
+
+            let g(b:Complex,d:Complex)=
+                (b-z-d)/(b-z+d)
+
+            let d(b:Complex,u:Complex)=
+                Complex.Sqrt(Complex.Pow(b-z,2.0) - sigma2*(2.0*i*u*phi-phi**2.0))
+
+            let C(b:Complex,d:Complex,g:Complex):Complex=
+                let ratio = (1.0-g*Complex.Exp(-d*tau))/(1.0 - g)
+                (r-q)*i*phi*tau + a/sigma2 * ((b-z-d)*tau-2.0*Complex.Log(ratio))
+
+            let D(b:Complex,d:Complex,g:Complex)=
+                (b-z-d)/(sigma2)*(1.0-Complex.Exp(-d*tau))/(1.0-g*Complex.Exp(-d*tau))
+
+            let d1 = d(b1,u1)
+            let d2 = d(b2,u2)
+            let g1 = g(b1,d1)
+            let g2 = g(b2,d2)
+            let C1 = C(b1,d1,g1)
+            let C2 = C(b2,d2,g2)
+            let D1 = D(b1,d1,g1)
+            let D2 = D(b2,d2,g2)
+            
+            let f1 = (Complex.Exp(C1+D1*vo+i*phi*logS))
+            let f2 = (Complex.Exp(C2+D2*vo+i*phi*logS))
+            
+            let Ia = Complex.Exp(-i*phi*logK)/(i*phi)*(S*Complex.Exp(-q*tau)*f1-K*Complex.Exp(-r*tau)*f2)
+
+            Ia.Real
+
+
+        let I=GaussLegendreRule.Integrate(Func<float,float>(integrand), 0.0, 100.0, 5);
+        let c = 0.5*S*Math.Exp(-q*tau)-0.5*K*Math.Exp(-r*tau)+1.0/Math.PI*I
+        if call then                       
+            c
+        else
+            c+K*Math.Exp(-r*tau)-S*Math.Exp(-q*tau)
+    
+
+
+
